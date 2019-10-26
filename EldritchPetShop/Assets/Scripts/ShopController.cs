d1badbd59f963d49ed56eb7b11f86a26645a29a2
Micah Erb <merb1000@gmail.com>
2019-10-26 11:34:56 -0400

    Updated Shop Classes

--- EldritchPetShop/Assets/Scripts/ShopController.cs
+++ EldritchPetShop/Assets/Scripts/ShopController.cs
@@ -7,26 +7,41 @@ public class ShopController : MonoBehaviour
     public float Price;
     public bool Owned;
     public string Name;
-    public PetController Controller;
+    public PetController PC;
+    public bool hidden;
+    public ShopManager SM;
+    public bool Purchasing;
 
     void Start()
     {
-        Name = Controller.petName;
-        Price = Controller.price;
+        Name = PC.petName;
+        Price = PC.price;
         Owned = false;
+        Purchasing = false;
+        hidden = true;
     }
 
     void Update()
     {
-        //if shop button clicked and owned = false
-            //show purchase button
-        //
-        //if purchased button clicked
+        hidden = SM.HideShop;
+        if (hidden == false & Owned == false)
+        {
+            //Show Monster button
+        }
+        if (Purchasing == true)
+        {
             Owned = true;
-            //hide purchase button
-        //
-        //if exit button clicked
-            //hide purchase button
-        //
+            //Hide Monster Button
+            //Add Monster to House
+        }
+        if (hidden == true)
+        {
+            //Hide Monster Button
+        }
+    }
+
+    public void PetButtonPress()
+    {
+        Purchasing = true;
     }
 }
--- EldritchPetShop/Assets/Scripts/ShopManager.cs
+++ EldritchPetShop/Assets/Scripts/ShopManager.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class ShopManager : MonoBehaviour
 {
+    public bool HideShop;
 
     void Start()
     {
-
+        HideShop = true;
     }
 
     void Update()
     {
-        
+        if(HideShop == false)
+        {
+            //Display Shop UI
+        }
+        if (HideShop == true)
+        {
+            //Hide Shop UI
+        }
     }
 
+    public void ButtonPress()
+    {
+        HideShop = false;
+    }
+
+    public void ExitButtonPress()
+    {
+        HideShop = true;
+    }
 }
