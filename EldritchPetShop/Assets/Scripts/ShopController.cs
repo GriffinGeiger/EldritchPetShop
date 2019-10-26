3574ce732e1f58ab6bb975410a9f539c4d2c152f
Micah Erb <merb1000@gmail.com>
2019-10-26 11:08:42 -0400

    Added Shop Controller

---
+++ EldritchPetShop/Assets/Scripts/ShopController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopController : MonoBehaviour
+{
+    public float Price;
+    public bool Owned;
+    public string Name;
+    public PetController Controller;
+
+    void Start()
+    {
+        Name = Controller.petName;
+        Price = Controller.price;
+        Owned = false;
+    }
+
+    void Update()
+    {
+        //if shop button clicked and owned = false
+            //show purchase button
+        //
+        //if purchased button clicked
+            Owned = true;
+            //hide purchase button
+        //
+        //if exit button clicked
+            //hide purchase button
+        //
+    }
+}
