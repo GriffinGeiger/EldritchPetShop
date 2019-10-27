853061b4d34e091bca96bd9edda0c2b731a49b8c
Griffin Geiger <griffin2420@yahoo.com>
2019-10-27 12:53:56 -0400

    Offering inventory lowers when you consume an offering

--- EldritchPetShop/Assets/Scripts/OfferingDispenser.cs
+++ EldritchPetShop/Assets/Scripts/OfferingDispenser.cs
@@ -9,10 +9,16 @@ public class OfferingDispenser : MonoBehaviour, IPointerDownHandler
     public Transform spawnLocation;
     public Camera mainCam;
     public GameObject offeringDisplay;
-
+    public GUIManager GUI;
+    public Offering offeringType;
     void Start()
     {
         mainCam = Camera.main;
+    }
+
+    private void Awake()
+    {
+        GUI = (GUIManager)FindObjectOfType<GUIManager>();
     }
 
 
@@ -33,6 +39,23 @@ public class OfferingDispenser : MonoBehaviour, IPointerDownHandler
         GameObject go = GameObject.Instantiate(offeringPrefab, mainCam.ScreenToWorldPoint(eventData.position), Quaternion.identity);
         go.transform.position = new Vector3(go.transform.position.x, go.transform.position.y, 0);
         go.GetComponentInChildren<DragObject>().offeringDisplay = offeringDisplay;
+        switch (offeringType)
+        {
+            case Offering.Human:
+                GUI.Humans--;
+                break;
+            case Offering.Animal:
+                GUI.Animals--;
+                break;
+            case Offering.Relic:
+                GUI.Relics--;
+                break;
+            case Offering.Random:
+                Debug.LogWarning("Don't make offering type random in OfferingDispenser");
+                break;
+            default:
+                break;
+        }
         offeringDisplay.SetActive(false);
     }
 }
